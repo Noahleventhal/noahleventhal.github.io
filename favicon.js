090b21a3a8a1ac8a7eccdb153e53797b425193c6
Noah Leventhal <noahshmjds@gmail.com>
2026-01-10 22:29:26 -0600

    test

--- favicon.js
+++ favicon.js
@@ -7,6 +7,9 @@ faviconLink.type = 'image/x-icon';
 faviconLink.rel = 'shortcut icon';
 document.getElementsByTagName('head')[0].appendChild(faviconLink);
 
+const bgImage = new Image();
+bgImage.src = 'favicon-bg.png';
+
 const fonts = [
     'Inter', 'Times New Roman', 'Courier New', 'Brush Script MT',
     'Copperplate', 'Papyrus', 'Comic Sans MS', 'Impact', 'Georgia',
@@ -17,46 +20,25 @@ const fonts = [
 
 let fontIndex = 0;
 
-// Start Loop Immediately
-setInterval(() => {
-    // 1. Clear
-    faviconCtx.clearRect(0, 0, 64, 64);
+bgImage.onload = function () {
+    setInterval(() => {
+        // 1. Draw Background Tile
+        faviconCtx.clearRect(0, 0, 64, 64);
+        faviconCtx.drawImage(bgImage, 0, 0, 64, 64);
 
-    faviconCtx.save();
+        // 2. Draw 'N'
+        faviconCtx.fillStyle = '#00ff00'; // Green
+        faviconCtx.textAlign = 'center';
+        faviconCtx.textBaseline = 'middle';
 
-    // 2. Rounded Clip
-    faviconCtx.beginPath();
-    const r = 24;
-    faviconCtx.moveTo(r, 0);
-    faviconCtx.lineTo(64 - r, 0);
-    faviconCtx.quadraticCurveTo(64, 0, 64, r);
-    faviconCtx.lineTo(64, 64 - r);
-    faviconCtx.quadraticCurveTo(64, 64, 64 - r, 64);
-    faviconCtx.lineTo(r, 64);
-    faviconCtx.quadraticCurveTo(0, 64, 0, 64 - r);
-    faviconCtx.lineTo(0, r);
-    faviconCtx.quadraticCurveTo(0, 0, r, 0);
-    faviconCtx.closePath();
-    faviconCtx.clip();
+        // Pick font
+        let font = fonts[fontIndex];
+        fontIndex = (fontIndex + 1) % fonts.length;
 
-    // 3. Fill Yellow (More vibrant than beige)
-    faviconCtx.fillStyle = '#F4D03F';
-    faviconCtx.fillRect(0, 0, 64, 64);
+        faviconCtx.font = `bold 40px "${font}"`;
+        faviconCtx.fillText('N', 32, 34); // Slight vertical offset to center visually
 
-    // 4. Draw 'N'
-    faviconCtx.fillStyle = 'black';
-    faviconCtx.textAlign = 'center';
-    faviconCtx.textBaseline = 'middle';
-
-    let font = fonts[fontIndex];
-    fontIndex = (fontIndex + 1) % fonts.length;
-
-    faviconCtx.font = `bold 40px "${font}"`;
-    faviconCtx.fillText('N', 32, 34);
-
-    faviconCtx.restore();
-
-    // 5. Update Link
-    faviconLink.href = faviconCanvas.toDataURL('image/png');
-
-}, 1000);
+        // 3. Update Favicon
+        faviconLink.href = faviconCanvas.toDataURL('image/png');
+    }, 1000); // Cycle every second
+};
